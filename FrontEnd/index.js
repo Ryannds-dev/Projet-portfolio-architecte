d275c0a20bbfebc38817ec9bd0df2c264e6f1614
Ryann <154708766+Ryannds-dev@users.noreply.github.com>
2025-10-05 21:28:12 +0200

    SCRUM-14 feat: fetch works from backend and display in gallery

---
+++ FrontEnd/index.js
@@ -0,0 +1,27 @@
+// RECUPERATION DES TRAVAUX DE L'ARCHITECTE GRACE A L'API
+
+const URL_WORKS = "http://localhost:5678/api/works";
+const gallery = document.querySelector(".gallery");
+
+async function loadWorks() {
+  const reponseAPI = await fetch(URL_WORKS);
+  const works = await reponseAPI.json();
+  console.log(works);
+
+  works.forEach((work) => {
+    const figure = document.createElement("figure");
+
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+
+    gallery.appendChild(figure);
+  });
+}
+loadWorks();
